67a2297405a5ba54b70c2642c42b8186d2b00c97
José Maykol <jpaniura@unsa.edu.pe>
2023-12-09 23:56:02 -0500

    BUILD: add CORS, morgan, and reading control routes

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const app = express()
 
 require('./database/db')
 
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!')
+  res.send('Control de lectura')
 })
+
+const readingControlRoutes = require('./routes/readingControl.routes')
+app.use('/reading-control', readingControlRoutes)
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en el puerto 3000')
