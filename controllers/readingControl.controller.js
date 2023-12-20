10a7ec4af99bf1de140c2f5cb4862229b027f681
José Maykol <jpaniura@unsa.edu.pe>
2023-12-19 23:55:50 -0500

    FEATURE: add calculate score  to calculate reading assessment score

--- controllers/readingControl.controller.js
+++ controllers/readingControl.controller.js
@@ -2,11 +2,12 @@ const { ReadingControl } = require('../models/readingControl.model')
 
 const addReadingControl = async (req, res) => {
   try {
-    const { title, text, questions } = req.body
+    const { title, text, questions, total_questions } = req.body
 
     const newReading = new ReadingControl({
       title,
       text,
+      total_questions,
       questions
     })
 
@@ -60,10 +61,62 @@ const deleteReadingControl = async (req, res) => {
   }
 }
 
+const calculateScore = async (req, res) => {
+  try {
+    const studentAnswers = req.body.answers
+    const readingAssessment = await ReadingControl.findById(req.params.id)
+
+    let score = 0
+    let correctQuestions = 0
+    let incorrectQuestions = 0
+    let totalQuestions = 0
+
+    const results = []
+
+    for (const question of readingAssessment.questions) {
+        const studentAnswer = studentAnswers.find(answer => answer.question_id === question.question_id);
+
+        if (studentAnswer) {
+            totalQuestions++;
+
+            const correctAnswer = question.correct_answer_text;
+            const studentAnswerText = studentAnswer.answer_text;
+
+            const isCorrect = correctAnswer.toLowerCase() === studentAnswerText.toLowerCase(); // Compare ignoring case
+
+            if (isCorrect) {
+                score += 1;
+                correctQuestions++;
+            } else {
+                incorrectQuestions++;
+            }
+
+            results.push({
+                question_id: question.question_id,
+                correct: isCorrect,
+            });
+        }
+    }
+
+    const scoreOutOf20 = (score / totalQuestions) * 20;
+
+    res.json({
+        results,
+        score: scoreOutOf20,
+        correctQuestions,
+        incorrectQuestions,
+        totalQuestions,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+}
+
 module.exports = {
   getAllReadingControl,
   getReadingControlById,
   addReadingControl,
   updateReadingControl,
-  deleteReadingControl
+  deleteReadingControl,
+  calculateScore
 }
--- routes/readingControl.routes.js
+++ routes/readingControl.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { addReadingControl, getAllReadingControl, getReadingControlById, updateReadingControl } = require('../controllers/readingControl.controller')
+const { addReadingControl, getAllReadingControl, getReadingControlById, updateReadingControl, calculateScore } = require('../controllers/readingControl.controller')
 
 const router = Router()
 
@@ -7,5 +7,6 @@ router.get('/', getAllReadingControl)
 router.get('/:id', getReadingControlById)
 router.post('/', addReadingControl)
 router.put('/:id', updateReadingControl)
+router.post('/:id/calculate_score',calculateScore)
 
 module.exports = router
